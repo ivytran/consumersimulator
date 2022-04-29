d157e90c806bf2d18e3b9f7c99107adece1f2bae
Angelique <angelicrf@gmail.com>
2022-04-29 11:22:29 -0700

    cart added - box cols on statics

--- consumersimulator/Assets/ModernSupermarket/Scripts/StaticStatus.cs
+++ consumersimulator/Assets/ModernSupermarket/Scripts/StaticStatus.cs
@@ -1,17 +1,60 @@
-using UnityEditor;
+using System.Collections;
 using UnityEngine;
 
 public class StaticStatus : MonoBehaviour
 {
 
+    private string getCartName;
+    private string stringItem = "";
+    private int thisLength = 0;
     public void GrabStaticStatSelectStart()
     {
-        var start = gameObject.GetComponent<Rigidbody>().isKinematic = true;
-        Debug.Log( "select started" + start );
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
     }
     public void GrabStaticStatSelectFinish()
     {
-        var finsigh = gameObject.GetComponent<Rigidbody>().isKinematic = false;
-        Debug.Log( "select finished" + finsigh );
+        StartCoroutine( ObjectCartCo() );
+    }
+    public void CartKinematic()
+    {
+        getCartName = Carts.cart;
+        GameObject.Find( getCartName ).GetComponent<Rigidbody>().isKinematic = true;
+    }
+    public void CartNonKinematic()
+    {
+        getCartName = Carts.cart;
+        GameObject.Find( getCartName ).GetComponent<Rigidbody>().isKinematic = false;
+    }
+    public void CartParentObject()
+    {
+        getCartName = Carts.cart;
+        Debug.Log( "cartName " + getCartName );
+        gameObject.transform.parent = GameObject.Find( getCartName ).transform;
+        Carts.isItemCart = true;
+        CartItems.ItemName = gameObject.name;
+        stringItem = CartItems.ItemName.Split( '_' )[0];
+        CartItems.AddItems( stringItem );
+        CartItems.TotalItems = CartItems.HoldItems.Count;
+        //items Objects
+        if (CartItems.ItemCall.Count == 0)
+        {
+            thisLength = 1;
+        }
+        else
+        {
+            thisLength = CartItems.ItemCall.Count;
+        }
+        for (int i = 0; i < thisLength; i++)
+        {
+            CartItems.AddItemsObject( i , stringItem , $"{stringItem} Desc" );
+            Debug.Log( "itemsObject " + CartItems.ItemCall[i].Name + CartItems.ItemCall[i].Id + CartItems.ItemCall[i].Description );
+        }
+
+    }
+    private IEnumerator ObjectCartCo()
+    {
+        gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        yield return new WaitForSeconds( 2f );
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
---
+++ consumersimulator/Assets/Scripts/CartItems.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+
+public class CartItems
+{
+    private static string itemName;
+    private static int itemCount;
+    private static List<string> allItems = new List<string>();
+    private static List<Item> itemsObject = new List<Item>();
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+    public static string ItemName
+    {
+        get{ return itemName; }
+        set { itemName = value; }
+    }
+    
+    public static List<Item> ItemCall
+    {
+        get { return itemsObject; }
+    }
+    public static List<string> HoldItems{ get { return allItems; }}
+    public static void AddItems(string argValue)
+    {
+        allItems.Add( argValue );
+    }
+    public static void AddItemsObject(int thisId,string thisName, string thisDesc)
+    {
+        itemsObject.Add( new Item { Id = thisId , Name = thisName , Description = thisDesc } );
+    }
+    public static int TotalItems
+    {
+        get { return itemCount; }
+        set { itemCount = value; }
+    }
+}
