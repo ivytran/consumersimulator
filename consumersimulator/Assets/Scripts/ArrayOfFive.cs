2fc7e4895c8589f485ef9cb17f835d692a54db5a
ValentinkaM <32400353+ValentinkaM@users.noreply.github.com>
2022-05-26 12:13:14 -0700

    Added button OnClick Event

--- consumersimulator/Assets/Scripts/ArrayOfFive.cs
+++ consumersimulator/Assets/Scripts/ArrayOfFive.cs
@@ -2,32 +2,45 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ArrayOfFive : MonoBehaviour
 {
     public GameObject[] fiveitems; //will attach other objects in Unity to this one
-    public string[] arrayoffive =
-    {
-        "Mango",
-        "Pasta",
-        "Sliced Bread",
-        "Apple",
-        "Wine"
-
-    };
+    public Button myButton;
+    //public string[] arrayoffive =
+    //{
+    //    "Mango",
+    //    "Pasta",
+    //    "Sliced Bread",
+    //    "Apple",
+    //    "Wine"
+    //};
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < arrayoffive.Length; i++)
+        Button btn = myButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+      
+    }
+    void TaskOnClick()
+    {
+        for (int i = 0; i < fiveitems.Length; i++)
         {
             int rank = i + 1;
-            Console.Write(rank + "." + arrayoffive[i]);
+            Console.Write(rank + "." + fiveitems[i]);
         }
+        Debug.Log("You clicked the button!");
     }
+    //// Update is called once per frame
+    //void Update()
+    //{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //}
+
+    //public void buttonClick(Button myButton)
+    //{
+    //    btn.onClick.AddListener(buttonClick);
+
+    //}
 }
