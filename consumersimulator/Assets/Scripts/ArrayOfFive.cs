069df79c8935a4152d03992a9dbbb36b0a7d839e
ValentinkaM <32400353+ValentinkaM@users.noreply.github.com>
2022-05-26 23:41:27 -0700

    5array UI location changed

--- consumersimulator/Assets/Scripts/ArrayOfFive.cs
+++ consumersimulator/Assets/Scripts/ArrayOfFive.cs
@@ -6,30 +6,32 @@ using UnityEngine.UI;
 
 public class ArrayOfFive : MonoBehaviour
 {
-    public GameObject[] fiveitems; //will attach other objects in Unity to this one
+    //public GameObject[] fiveitems; //will attach other objects in Unity to this one
     public Button myButton;
-    //public string[] arrayoffive =
-    //{
-    //    "Mango",
-    //    "Pasta",
-    //    "Sliced Bread",
-    //    "Apple",
-    //    "Wine"
-    //};
+    public string[] arrayoffive =
+{
+        "Mango",
+        "Pasta",
+        "Sliced Bread",
+        "Apple",
+        "Wine"
+    };
     // Start is called before the first frame update
     void Start()
     {
-        Button btn = myButton.GetComponent<Button>();
-        btn.onClick.AddListener(TaskOnClick);
-      
-    }
-    void TaskOnClick()
-    {
-        for (int i = 0; i < fiveitems.Length; i++)
+        for (int i = 0; i < arrayoffive.Length; i++)
         {
             int rank = i + 1;
-            Console.Write(rank + "." + fiveitems[i]);
+            Console.Write(rank + "." + arrayoffive[i]);
         }
+    }
+          
+    
+    void TaskOnClick()
+    {
+        Button btn = myButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+     
         Debug.Log("You clicked the button!");
     }
     //// Update is called once per frame
